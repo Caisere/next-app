a748d1a56c1c3f3607ea55d96c4d1aa498b50587
Omoshola <heylehgbedey@gmail.com>
2025-06-16 15:32:55 -0700

    initializing next-app

---
+++ app/about/layout.jsx
@@ -0,0 +1,16 @@
+
+
+export const metadata = {
+    title: "About Us",
+    description: "About Us Page",
+};
+
+export default function About({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <div className="">
+            <h1>Home Page</h1>
+            <p>This is the Home Page</p>
+        </div>
+    );
+}
+
+export default Home
